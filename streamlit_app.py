d2ce46dd9c6e6c6a9d378ed0a4c7366f5f6514f7
Gobinath Ramaraj <120233871+tamilkumara@users.noreply.github.com>
2022-12-09 23:05:45 -0500

    Update streamlit_app.py
    Text added

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,6 @@ import streamlit
 
 streamlit.title('My parents New Health Diner')
 streamlit.header('Breakfast Menu')
+st.text('Omega 3 & Blueberry Oatmeal')
+st.text('Kale, Spinach & Rocket Smoothie')
+st.text('Hard-Boiled Free-Range Egg')
