a311f5df5314c31a38ccbde246697568fdca826d
Gobinath Ramaraj <120233871+tamilkumara@users.noreply.github.com>
2022-12-09 23:07:54 -0500

    Update streamlit_app.py
    change all st to streamlit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,9 @@
 import streamlit
 
 streamlit.title('My parents New Health Diner')
+
 streamlit.header('Breakfast Menu')
+
 streamlit.text('Omega 3 & Blueberry Oatmeal')
-st.text('Kale, Spinach & Rocket Smoothie')
-st.text('Hard-Boiled Free-Range Egg')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
