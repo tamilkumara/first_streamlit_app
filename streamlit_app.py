74278af741d98032ee2aaba4e6b61263ca85239f
Gobinath Ramaraj <120233871+tamilkumara@users.noreply.github.com>
2022-12-09 23:13:32 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 
 streamlit.title('My parents New Health Diner')
 
-streamlit.header('Breakfast Menu')
+streamlit.header('Breakfast Favourites')
 
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
