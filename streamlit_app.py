4b1b1a05b89775a07bcecf937e7123b1d8b09a40
Gobinath Ramaraj <120233871+tamilkumara@users.noreply.github.com>
2022-12-10 19:33:37 -0500

    Update streamlit_app.py
    correct error in '.loc'

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,5 +16,5 @@ my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.co
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
 fruits_selected = streamlit.multiselect("Pick some Fruits",list(my_fruit_list.index),['Avocado','Strawberries'])
-fruits_to_show = my_fruit_list.loc(fruits_selected)
+fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
