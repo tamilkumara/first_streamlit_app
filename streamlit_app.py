9d7f12eaefdb0461afee79a6b00a81d8b698bebd
Gobinath Ramaraj <120233871+tamilkumara@users.noreply.github.com>
2022-12-15 12:33:11 -0500

    Update streamlit_app.py
    added header/text

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,11 @@ import streamlit
 import pandas
 
 streamlit.title('My parents New Health Diner')
+streamlit.header('Breakfast Favourites')
+
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avacado Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
