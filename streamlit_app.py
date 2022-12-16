5645f53f97bc380250e90013f9b09443ca5e89de
Gobinath Ramaraj <120233871+tamilkumara@users.noreply.github.com>
2022-12-15 23:52:09 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,27 +1,28 @@
-import streamlit
-import pandas
+import streamlit as st
+import pandas as pd
 
-streamlit.title('My parents New Health Diner')
-streamlit.header('Breakfast Favourites')
+st.title('My parents New Health Diner')
+st.header('Breakfast Favourites')
 
-streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
-streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
-streamlit.text('🐔 Hard-Boiled Free-Range Egg')
-streamlit.text('🥑🍞 Avacado Toast - For Muthu')
+st.text('🥣 Omega 3 & Blueberry Oatmeal')
+st.text('🥗 Kale, Spinach & Rocket Smoothie')
+st.text('🐔 Hard-Boiled Free-Range Egg')
+st.text('🥑🍞 Avacado Toast - For Muthu')
 
-streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
 
-fruits_selected = streamlit.multiselect("Pick some Fruits",list(my_fruit_list.index),['Avocado','Strawberries'])
+fruits_selected = st.multiselect("Pick some Fruits",list(my_fruit_list.index),['Avocado','Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 
-df_my_fruit_list = pandas.DataFrame(fruits_to_show)
-streamlit.dataframe(df_my_fruit_list)
+df_my_fruit_list = pd.DataFrame(fruits_to_show)
 
+st.dataframe(df_my_fruit_list)
+st.text(df_my_fruit_list)
 #streamlit.text(fruits_to_show)
 
 #streamlit.dataframe[my_fruit_list]
