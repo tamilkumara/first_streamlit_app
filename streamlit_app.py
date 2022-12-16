a12ad939a50b70df26199bf72662a589aad0d930
Gobinath Ramaraj <120233871+tamilkumara@users.noreply.github.com>
2022-12-15 23:48:55 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,15 +12,16 @@ streamlit.text('🥑🍞 Avacado Toast - For Muthu')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-my_fruit_list1 = my_fruit_list.set_index('Fruit')
-#streamlit.text(my_fruit_list1)
-pandas.DataFrame(my_fruit_list1)
-pandas.DataFrame(my_fruit_list)
+my_fruit_list = my_fruit_list.set_index('Fruit')
 
 
-import streamlit
-fruits_selected = streamlit.multiselect("Pick some Fruits",list(my_fruit_list1.index),['Avocado','Strawberries'])
-fruits_to_show = my_fruit_list1.loc[fruits_selected]
-streamlit.text(fruits_to_show)
-streamlit.DataFrame(my_fruit_list1)
+fruits_selected = streamlit.multiselect("Pick some Fruits",list(my_fruit_list.index),['Avocado','Strawberries'])
+fruits_to_show = my_fruit_list.loc[fruits_selected]
+
+
+df_my_fruit_list = pandas.DataFrame(fruits_to_show)
+streamlit.dataframe(df_my_fruit_list)
+
+#streamlit.text(fruits_to_show)
+
 #streamlit.dataframe[my_fruit_list]
