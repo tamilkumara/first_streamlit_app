b3e4b6fae5e7c0b865acb209a1963382dbf7425e
Gobinath Ramaraj <120233871+tamilkumara@users.noreply.github.com>
2022-12-16 09:17:44 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,17 +12,18 @@ st.text('🥑🍞 Avacado Toast - For Muthu')
 st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-my_fruit_list = my_fruit_list.set_index('Fruit')
+st.text(my_fruit_list)
+#my_fruit_list = my_fruit_list.set_index('Fruit')
 
 
-fruits_selected = st.multiselect("Pick some Fruits",list(my_fruit_list.index),['Avocado','Strawberries'])
-fruits_to_show = my_fruit_list.loc[fruits_selected]
+#fruits_selected = st.multiselect("Pick some Fruits",list(my_fruit_list.index),['Avocado','Strawberries'])
+#fruits_to_show = my_fruit_list.loc[fruits_selected]
 
+#st.text(fruits_to_show)
+#df_my_fruit_list = pd.DataFrame(fruits_to_show)
 
-df_my_fruit_list = pd.DataFrame(fruits_to_show)
-
-st.dataframe(df_my_fruit_list)
-st.text(df_my_fruit_list)
+#st.dataframe(df_my_fruit_list)
+#st.text(df_my_fruit_list)
 #streamlit.text(fruits_to_show)
 
 #streamlit.dataframe[my_fruit_list]
